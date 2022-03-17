5cee9960e679a2996e8e0ac678112735a78aa458
DevWithReact <67072920+DevWithReact@users.noreply.github.com>
2022-03-17 16:25:21 +0300

    add tab icons

--- App/navigators/HomeNavigator.js
+++ App/navigators/HomeNavigator.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import {
+  Image,
+} from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ConsumerProfileScreen from '../containers/Consumer/Profile';
 import ConsumerMailScreen from '../containers/Consumer/Mail';
@@ -6,31 +9,66 @@ import ConsumerWatchScreen from '../containers/Consumer/Watch';
 import DentistMailScreen from '../containers/Dentist/Mail';
 import DentistProfileScreen from '../containers/Dentist/Profile';
 import ChannelScreen from '../containers/Channel';
+import Images from '../utils/Images';
 
 
 const Tab = createBottomTabNavigator();
 
+const consumerTabs = [
+    {
+        name: "Mail",
+        icon: "ic_chat",
+        component: ConsumerMailScreen
+    }, {
+        name: "Chat",
+        icon: "ic_chat",
+        component: ChannelScreen
+    }, {
+        name: "Watch",
+        icon: "ic_watch",
+        component: ConsumerWatchScreen
+    }, {
+        name: "Profile",
+        icon: "ic_user",
+        component: ConsumerProfileScreen
+    }
+];
+const dentistTabs = [
+    {
+        name: "Mail",
+        icon: "ic_mail",
+        component: DentistMailScreen
+    }, {
+        name: "Chat",
+        icon: "ic_chat",
+        component: ChannelScreen
+    }, {
+        name: "Profile",
+        icon: "ic_user",
+        component: DentistProfileScreen
+    }
+]
 const HomeNavigator = () => {
     const isConsumer = false;
+    const activeTabs = 
+        isConsumer ? consumerTabs : dentistTabs;
+
     return (
         <Tab.Navigator
             screenOptions={{ headerShown: false }}
         >
         {
-            isConsumer ? (
-            <>
-                <Tab.Screen name="Mail" component={ConsumerMailScreen} />
-                <Tab.Screen name="Chat" component={ChannelScreen} />
-                <Tab.Screen name="Watch" component={ConsumerWatchScreen} />
-                <Tab.Screen name="Profile" component={ConsumerProfileScreen} />
-            </>
-            ) : (
-            <>
-                <Tab.Screen name="Mail" component={DentistMailScreen} />
-                <Tab.Screen name="Chat" component={ChannelScreen} />
-                <Tab.Screen name="Profile" component={DentistProfileScreen} />
-            </>
-            )
+            activeTabs.map(t => (
+                <Tab.Screen
+                    key={t.name}
+                    name={t.name}
+                    component={t.component}
+                    options={{                            
+                        tabBarShowLabel: false,
+                        tabBarIcon: ({focused, color}) => <Image source={Images[focused ? t.icon : `${t.icon}_inactive`]}/>
+                    }}
+                />
+            ))
         }
         </Tab.Navigator>
     );
